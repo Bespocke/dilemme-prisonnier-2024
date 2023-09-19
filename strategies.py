5ab2dc27b9d9fb20d035d316d5e2e94ae9402461
Bespocke <138402452+Bespocke@users.noreply.github.com>
2023-09-19 20:11:37 +0200

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -1,1 +1,47 @@
+def cian(i,moi,lui,elle,memoire):
+    if i==0 :
+        memoire.append(0)
+        return 1
+    if lui[i-1]==1 and elle[i-1]==1 :
+        return 1
+    if lui[i-1]+elle[i-1]==1  :
+        memoire[0]+=1
+        if memoire[0]%3!=0 :
+            return 1
+        else :
+            return 0
 
+    else :
+        return 0
+
+def pasnils(i,moi,lui,elle,memoire):
+    if i==0 :
+        return 1
+    if lui[i-1]==1 and elle[i-1]==1 :
+        return 1
+    else:
+        return 0
+
+def nilsnonplus(i,moi,lui,elle,memoire):
+    if i==0 :
+        return 1
+    if lui[i-1]+elle[i-1]>0  :
+        return 1
+    else :
+        return 0
+
+def avare(i,moi,lui,elle,memoire):
+    if i==0:
+        return 1
+    if i==1:
+        return 1
+    if lui[i-1]+elle[i-1]==2 :
+        if lui[i-2]+elle[i-2]>0 :
+            return 0
+        else:
+            return 1
+
+    if lui[i-1]+elle[i-1]==1 :
+        return rd.randint(0,1)
+    if lui[i-1]+elle[i-1]==0 :
+        return 0
